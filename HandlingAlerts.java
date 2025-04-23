00ac8f5bd21e285890b52616e36958aa6f8ef667
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-23 22:31:49 +0800

    Add files via upload

---
+++ HandlingAlerts.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingAlerts 
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		String[] names = {"JC", "Melo"};
+		driver.findElement(By.id("name")).sendKeys(names[0]);
+		driver.findElement(By.cssSelector("#alertbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		
+		driver.findElement(By.id("name")).sendKeys(names[1]);
+		driver.findElement(By.cssSelector("#confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+
+	}
+}
